41209ac036d93975f2cd49c610bc053f7ec27a5b
Ivan-Zelyapugin <147281940+Ivan-Zelyapugin@users.noreply.github.com>
2024-01-24 23:06:47 +0300

    12 - 13 работы по с#
    Сравнение эффективности различных методов сортировки.

---
+++ lb12-13/lb12-13/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace lb12_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.sort = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(-1, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(803, 385);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Сравнения";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Перестановки";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Время";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.sort);
+            this.panel1.Controls.Add(this.Exit);
+            this.panel1.Location = new System.Drawing.Point(-1, 383);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(803, 69);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(223, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Размер массива";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(357, 20);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // sort
+            // 
+            this.sort.Location = new System.Drawing.Point(24, 20);
+            this.sort.Name = "sort";
+            this.sort.Size = new System.Drawing.Size(134, 23);
+            this.sort.TabIndex = 1;
+            this.sort.Text = "Сортировать";
+            this.sort.UseVisualStyleBackColor = true;
+            this.sort.Click += new System.EventHandler(this.sort_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(663, 20);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 0;
+            this.Exit.Text = "Выход";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Зеляпугин Иван";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button sort;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ lb12-13/lb12-13/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lb12_13
+{
+    public partial class Form1 : Form
+    {
+        
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = 7;       
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.Rows[0].Cells[1].Value = "Обмен";
+            dataGridView1.Rows[1].Cells[1].Value = "Выбор";
+            dataGridView1.Rows[2].Cells[1].Value = "Включение";
+            dataGridView1.Rows[3].Cells[1].Value = "Шелла";
+            dataGridView1.Rows[4].Cells[1].Value = "Быстрая";
+            dataGridView1.Rows[5].Cells[1].Value = "Линейная";
+            dataGridView1.Rows[6].Cells[1].Value = "Встроенная";
+            for (int i = 0; i < 7; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = false;
+            }
+
+        }
+        private int[] BubbleSort(int[] arr)
+        {
+            int compr = 0;
+            int replace = 0;
+            int start = Environment.TickCount;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                bool check = true;
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    compr++;
+                    if (arr[j] > arr[j + 1])
+                    {
+                        replace++;
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        check = false;
+                    }
+                }
+                if (check)
+                {
+                    break;
+                }
+            }
+            int res = Environment.TickCount - start;
+            int[] result = {compr, replace, res};
+            return result;
+
+        }
+
+        private int[] Select(int[] arr)
+        {
+            int compr = 0;
+            int replace = 0;
+            int start = Environment.TickCount;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int max = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    compr++;
+                    if (arr[j] > arr[max])
+                        max = j;
+
+                }
+                replace++;
+                int temp = arr[i];
+                arr[i] = arr[max];
+                arr[max] = temp;
+            }
+            int res = Environment.TickCount - start;
+            int[] result = {compr, replace, res };
+            return result;
+        }
+        private void sort_Click(object sender, EventArgs e)
+        {       
+            int N = (int)numericUpDown1.Value;
+            int[] mas = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                mas[i] = rnd.Next(0, N);
+            }
+
+            //Пузырёк
+            if ((bool)dataGridView1.Rows[0].Cells[0].Value)
+            {
+                int[] copy = (int[])mas.Clone();
+                int[] res = BubbleSort(copy);
+                dataGridView1.Rows[0].Cells[2].Value = res[0];
+                dataGridView1.Rows[0].Cells[3].Value = res[1];
+                dataGridView1.Rows[0].Cells[4].Value = res[2];
+            }
+            else
+            {
+                for (int i = 2; i < 5; i++)
+                {
+                    dataGridView1.Rows[0].Cells[i].Value = null;
+                }
+            }
+
+            //Прямой выбор
+            if ((bool)dataGridView1.Rows[1].Cells[0].Value)
+            {
+                int[] copy = (int[])mas.Clone();
+                int[] res = Select(copy);
+                dataGridView1.Rows[1].Cells[2].Value = res[0];
+                dataGridView1.Rows[1].Cells[3].Value = res[1];
+                dataGridView1.Rows[1].Cells[4].Value = res[2];
+            }
+            else
+            {
+                for (int i = 2; i < 5; i++)
+                {
+                    dataGridView1.Rows[1].Cells[i].Value = null;
+                }
+            }
+        }
+    }
+}
